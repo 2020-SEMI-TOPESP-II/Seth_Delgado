48b7e05c28a99ca27e1e678f053d512172719c1f
SADC06 <66226678+SADC06@users.noreply.github.com>
2020-08-01 21:23:21 -0400

    Add files via upload

---
+++ Laboratorio_7/app.js
@@ -0,0 +1,98 @@
+require('dotenv').config()
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const cors = require('cors')
+
+const connectDB  = require('./dbConfig');
+const Estudiantes = require ('./models/Estudiantes');
+
+const PORT = 3000;
+
+// Configuraciones
+app.set('view engine', 'pug');
+app.set('views', './views');
+
+//intermediarios
+app.use (bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cors());
+ 
+// Controladores - Views
+app.get('/estudiantes/', async (req, res) => {
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+    res.render('estudiantes', { estudiantes });
+});
+app.get('/estudiantes/:id', async (req, res) => {
+    try {
+        const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
+        res.render('estudiantes_detail', { estudiante });
+    } catch (error) {
+        console.log(error);
+        res.send('Error');
+    }
+});
+app.post('/estudiantes', async (req, res) => {
+    const { nombre, edad } = req.body;
+    await Estudiantes.create({ nombre, edad });
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+    res.render('estudiantes', { estudiantes, success: true });
+});
+
+
+//Controladores - API
+app.get('/api/estudiantes/', async (req, res) => {
+    const estudiantes = await Estudiantes.find().select('nombre edad');
+  res.json({
+        estudiantes,
+      cantidad: estudiantes.length
+    
+    });
+});
+
+app.post('/api/estudiantes/', async (req, res) => {
+    const { nombre, edad } = req.body;
+    await Estudiantes.create({ nombre, edad});
+    res.json({ nombre, edad});
+});
+
+app.get('/api/estudiantes/:id', async (req, res) => {
+    try{
+        const estudiante = await Estudiantes.findById(req.params.id).select('nombre edad');
+        res.json(estudiante);
+    } catch (error){
+        console.log(error);
+        res.json({});
+    }
+});
+
+app.put('/api/estudiantes/:id', async (req, res) => {
+    try{
+        const { nombre, edad }= req.body;
+        await Estudiantes.update({nombre, edad});
+        res.status(202).json({});
+    } catch (error) {
+        console.log(error);
+        res.json({});
+    }
+});
+
+app.delete('/api/estudiantes/:id', async (req, res) =>{
+    try {
+        await Estudiantes.findOneAndDelete(req.params.id);
+        res.status(202).json({});
+    } catch (error) {
+        console.log(error);
+        res.json({});
+    }
+});
+
+
+connectDB().then (() => {
+    app.listen(PORT, () => {
+        console.log(`Ejecutando en el puerto ${PORT}`);
+    });
+
+});
